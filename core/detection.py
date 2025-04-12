697d01e06fa01928c8765d11b2bb0c78c52ab46b
cofiprofim <121694687+cofiprofim@users.noreply.github.com>
2025-04-13 01:44:57 +0300

    Update detection.py

--- core/detection.py
+++ core/detection.py
@@ -1,35 +1,30 @@
-from typing import List, Iterable, Union
+from typing import List, Iterable, Optional
 
-from .utils import slice_list, Auth
-
-__all__ = (
-    "get_recent_sales", "get_users_thumbnails", "get_assets_thumbnails", 
-    "get_items_details", "get_user_inventory", "get_current_cap"
-)
-
-FAILED_IMAGE_URL = "https://i.ibb.co/Cs3Wvgb/7189017466763a9ed8874824aceba073.png"
+from .utils import slice_list
+from .clients import Auth
+from .constants import FAILED_IMAGE_URL
 
 
-async def get_recent_sales(auth: Auth) -> List[dict]:
-    async with auth.session.get(
-        f"https://economy.roblox.com/v2/users/{auth.user_id}/transactions?"
-        "cursor=&limit=10&transactionType=Sale&itemPricingType=PaidAndLimited",
-        ssl=False
+async def get_recent_sales(auth: Auth, *,
+                           limit: Optional[int] = 10) -> Optional[List[dict]]:
+    async with auth.get(
+        f"economy.roblox.com/v2/users/{auth.user_id}/transactions?"
+        f"cursor=&limit={limit}&transactionType=Sale&itemPricingType=PaidAndLimited"
     ) as response:
-            if response.status != 200:
-                return None
-        
-            return (await response.json()).get("data")
+        if response.status != 200:
+            return None
+
+        return (await response.json()).get("data")
 
 
-async def get_users_thumbnails(user_ids: Iterable[str], auth: Auth) -> Union[List[str], None]:
+async def get_users_thumbnails(user_ids: Iterable[str], auth: Auth) -> Optional[List[str]]:
     thumbnails = []
 
     for chunk in slice_list(user_ids, 100):
-        async with auth.session.get(
-            "https://thumbnails.roblox.com/v1/users/avatar-headshot?"
-            f"userIds={','.join(chunk)}&size=50x50&format=Png&isCircular=false",
-            ssl=False
+        processed_chunk = ','.join(chunk)
+        async with auth.get(
+            "thumbnails.roblox.com/v1/users/avatar-headshot?"
+            f"userIds={processed_chunk}&size=50x50&format=Png&isCircular=false"
         ) as response:
             data = (await response.json()).get("data")
 
@@ -40,19 +35,18 @@ async def get_users_thumbnails(user_ids: Iterable[str], auth: Auth) -> Union[Lis
 
             for user_id in user_ids:
                 if user_id in thumbnails_with_ids:
-                        thumbnails.append(thumbnails_with_ids[user_id])
+                    thumbnails.append(thumbnails_with_ids[user_id])
 
     return thumbnails
 
 
-async def get_assets_thumbnails(asset_ids: Iterable[str], auth: Auth) -> Union[List[str], None]:
+async def get_assets_thumbnails(asset_ids: Iterable[str], auth: Auth) -> Optional[List[str]]:
     thumbnails = []
 
     for chunk in slice_list(asset_ids, 100):
-        async with auth.session.get(
-            "https://thumbnails.roblox.com/v1/assets?"
-            f"assetIds={','.join(chunk)}&returnPolicy=PlaceHolder&size=50x50&format=Png&isCircular=false",
-            ssl=False
+        async with auth.get(
+            "thumbnails.roblox.com/v1/assets?"
+            f"assetIds={','.join(chunk)}&returnPolicy=PlaceHolder&size=50x50&format=Png&isCircular=false"
         ) as response:
             data = (await response.json()).get("data")
 
@@ -67,16 +61,16 @@ async def get_assets_thumbnails(asset_ids: Iterable[str], auth: Auth) -> Union[L
 
     return thumbnails
 
-async def get_items_details(item_ids: List[Union[int, str]], auth: Auth) -> List[dict]:
+
+async def get_items_details(item_ids: List[int], auth: Auth) -> List[dict]:
     items = []
 
     for chunk in slice_list(item_ids, 120):
         payload = {"items": [{"itemType": 1, "id": str(_id)} for _id in chunk]}
 
-        async with auth.session.post(
-            "https://catalog.roblox.com/v1/catalog/items/details",
-            json=payload,
-            ssl=False
+        async with auth.post(
+            "catalog.roblox.com/v1/catalog/items/details",
+            json=payload
         ) as response:
             data = (await response.json()).get("data")
 
@@ -91,15 +85,15 @@ async def get_items_details(item_ids: List[Union[int, str]], auth: Auth) -> List
 
     return items
 
+
 async def get_user_inventory(item_type: int, auth: Auth) -> List[dict]:
     assets = []
     cursor = ""
 
     while True:
-        async with auth.session.get(
-            f"https://inventory.roblox.com/v2/users/{auth.user_id}/inventory/{item_type}?"
-            f"limit=100&cursor={cursor}&sortOrder=Desc",
-            ssl=False
+        async with auth.get(
+            f"inventory.roblox.com/v2/users/{auth.user_id}/inventory/{item_type}?"
+            f"limit=100&cursor={cursor}&sortOrder=Desc"
         ) as response:
             if response.status != 200:
                 return assets
@@ -108,14 +102,14 @@ async def get_user_inventory(item_type: int, auth: Auth) -> List[dict]:
 
             cursor = data.get("nextPageCursor")
             assets.extend(
-                    [asset for asset in data.get("data") if asset.get("collectibleItemId") is not None])
+                    [asset for asset in data.get("data") if asset.get("serialNumber")])
 
             if not cursor:
                 return assets
 
-async def get_current_cap(auth: Auth) -> Union[dict, None]:
-    async with auth.session.get(
-        "https://itemconfiguration.roblox.com/v1/collectibles/metadata",
-        ssl=False
+
+async def get_current_cap(auth: Auth) -> Optional[dict]:
+    async with auth.get(
+        "itemconfiguration.roblox.com/v1/collectibles/metadata"
     ) as response:
         return (await response.json()).get("limitedItemPriceFloors")
