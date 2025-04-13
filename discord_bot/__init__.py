fa02d19ead1b5daaa347fb129ae401c2e2e83b6f
cofiprofim <121694687+cofiprofim@users.noreply.github.com>
2025-04-13 15:45:56 +0300

    Update __init__.py

--- discord_bot/__init__.py
+++ discord_bot/__init__.py
@@ -1,14 +1,23 @@
-from traceback import format_exc
-from discord.ext import commands
-from discord import app_commands
-import discord
+from __future__ import annotations
 
-from .buttons import ControlPanel
-from .embeds import exception_embed, custom_embed
+import traceback
+
+from discord.ext.commands import Bot, BadArgument, CommandNotFound, MissingRequiredArgument
+from discord import app_commands, Intents
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from main import AutoSeller
 
 
-async def start(self):
-    bot = commands.Bot(command_prefix=self.bot_prefix, intents=discord.Intents.all())
+from discord_bot.visuals.view import *
+from discord_bot.visuals.embeds import *
+from discord_bot.utils.decorators import *
+
+
+async def start(self: AutoSeller) -> None:
+    bot = Bot(command_prefix=self.bot_prefix, intents=Intents.all())
 
     @bot.event
     async def on_ready():
@@ -18,32 +27,28 @@ async def start(self):
     async def on_message_delete(message: discord.Message) -> None:
         if message == self.control_panel:
             self.control_panel = None
-    
+
     @bot.hybrid_command(name="start", description="Starts selling limiteds")
     @app_commands.describe(channel="The channel to send a control panel to")
-    async def start_command(ctx: discord.Message, channel: discord.TextChannel = None) -> None:
-        try:
-            await ctx.defer()
-            
-            channel = (channel or ctx.channel)
-            
-            if self.owners_list and ctx.author.id not in self.owners_list:
-                return await ctx.reply(content="You don't have permission to use this command!")
-            elif self.auto_sell:
-                return await ctx.reply(content=f"You can not run this command when you have auto sell enabled")
-            elif self.control_panel is not None:
-                return await ctx.reply(content=f"You already have one control panel running! {self.control_panel.service_message.jump_url}")
-            
-            await ControlPanel(self, channel, ctx).start()
-            return await ctx.reply(f"Successfully created a control panel {self.control_panel.service_message.jump_url}")
-        except:
-            return await ctx.reply(embed=exception_embed(format_exc()))
+    @users_blacklist(self.owners_list, message="You don't have permission to use this command!")
+    @base_command
+    async def start_command(ctx: discord.Message, channel: discord.TextChannel = None):
+        channel = (channel or ctx.channel)
+
+        if self.auto_sell:
+            return await ctx.reply(content=f"You can not run this command when you have auto sell enabled")
+        elif self.control_panel is not None:
+            return await ctx.reply(content=f"You already have one control panel running! "
+                                           f"({self.control_panel.service_message.jump_url})")
+
+        await ControlPanel(self, channel, ctx).start()
+        return await ctx.reply(f"Successfully created a control panel {self.control_panel.service_message.jump_url}")
 
     @bot.event
-    async def on_command_error(ctx: discord.Message, exception: Exception) -> None:
-        if type(exception) in (commands.BadArgument, commands.CommandNotFound, commands.MissingRequiredArgument):
+    async def on_command_error(ctx: discord.Message, exception: Exception):
+        if type(exception) in (BadArgument, CommandNotFound, MissingRequiredArgument):
             return None
 
-        await ctx.reply(embed=exception_embed(exception))
+        await ctx.reply(embed=exception_embed(traceback.format_exc()))
 
-    return await bot.start(self.bot_token)
+    await bot.start(self.bot_token)
